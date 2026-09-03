0fb8e0f5854f6a83111bfc4b004128930b03d694
Astrid Bjander <astridfrebja@gmail.com>
2026-09-03 07:57:53 +0200

    mer utfylling

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    console.log('page loaded');
+
+    document.documentElement.style.scrollBehavior = 'smooth';
+
+    document.querySelectorAll('.project-images').forEach(project => {
+        project.addEventListener('click', () => {
+            window.open(project.dataset.link, '_blank', 'noopener')
+        });
+    });
+
+});
