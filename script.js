287af8c14367345610750f28ff900af94d8b8011
Astrid Bjander <astridfrebja@gmail.com>
2026-09-06 21:30:54 +0200

    ramme

--- script.js
+++ script.js
@@ -5,9 +5,25 @@ document.addEventListener('DOMContentLoaded', function() {
     document.documentElement.style.scrollBehavior = 'smooth';
 
     document.querySelectorAll('.project-images').forEach(project => {
-        project.addEventListener('click', () => {
-            window.open(project.dataset.link, '_blank', 'noopener')
+
+        const video = project.querySelector('video');
+
+        if (video) {
+            video.addEventListener('click', () => {
+                if (video.paused) {
+                    video.play();
+                } else {
+                    video.pause();
+                }
+            });
+        }
+
+        project.addEventListener('click', (event) => {
+            if (event.target.tagName === 'VIDEO') return;
+
+            window.open(project.dataset.link, '_blank', 'noopener');
         });
+
     });
 
 });
